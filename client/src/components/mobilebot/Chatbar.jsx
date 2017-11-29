8227d3fbdee9b17c08d18a356adc3a86ce106e1d
howdycrystalm <howdycrystalm@gmail.com>
2017-11-29 12:38:16 -0800

    fine tuned hearder & chatbar icon positions

--- client/src/components/mobilebot/Chatbar.jsx
+++ client/src/components/mobilebot/Chatbar.jsx
@@ -74,7 +74,7 @@ class Chatbar extends React.Component {
               type="button"
               value="speak"
               // ref="rec" //a react attrib
-              className="mic"
+              className="mic-m"
               onClick={this.props.onClick.bind(this)}
             >
               <i className="material-icons" style={{ fontSize: '35px' }}>
@@ -100,7 +100,7 @@ class Chatbar extends React.Component {
               type="button"
               // ref="sendButton" //a react attrib
               value="send"
-              className="circle-x"
+              className="circle-m"
               onClick={this.props.onClick.bind(this)}
             >
               <i className="material-icons" style={{ fontSize: '20px' }}>
